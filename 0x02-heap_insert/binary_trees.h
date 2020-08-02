e044e51e46f9f01ce6330e64ba99c5fc1cc17c70
kaelwebdev <cdcp-1795@hotmail.com>
2020-08-02 07:12:43 -0500

    [Update] point 1 completed

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -26,4 +26,8 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+heap_t *exchange_order(heap_t *son);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
