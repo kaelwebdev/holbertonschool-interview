5b21354f1339411a9caf9ef9e50567c336359b56
kaelwebdev <cdcp-1795@hotmail.com>
2020-09-22 17:13:32 -0500

    [Fix] other way

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,45 +1,27 @@
 #!/usr/bin/python3
 """
-0x09. UTF-8 Validation
+0x09 UTF-8 Validation
 """
 
+#!/usr/bin/python3
 
-from itertools import takewhile
-
-
-def to_bs(bytes):
-    """
-    bytes -> bits
-    """
-    for byte in bytes:
-        n = []
-        e = 1 << 8
-        while e:
-            e >>= 1
-            n.append(bool(byte & e))
-            yield n
-
+'''UTF-8 validator'''
 
 def validUTF8(data):
     """
-    check valid UTF-8 encoding.
+    check valid UTF-8
     """
-    bs = to_bs(data)
-    for byte in bs:
-        if byte[0] == 0:
-            continue
-
-        a = sum(takewhile(bool, byte))
-        if a <= 1:
-            return False
-        if a >= 4:
-            return False
-
-        for _ in range(a - 1):
-            try:
-                byte = next(bs)
-            except StopIteration:
+    c = 0
+    for n in data:
+        m = 1 << 7
+        if not c:
+            while n & m:
+                c += 1
+                m >>= 1
+            if c > 4:
                 return False
-            if byte[0:2] != [1, 0]:
-                return False
-    return True
+        elif n >> 6 != 2:
+            return False
+        if c:
+            c -= 1
+    return c == 0
