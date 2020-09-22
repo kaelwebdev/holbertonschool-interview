22ed875158cc4c43a63ab11918d53b37ab587a06
kaelwebdev <cdcp-1795@hotmail.com>
2020-09-22 17:20:04 -0500

    [Fix] pep8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,25 +3,23 @@
 0x09 UTF-8 Validation
 """
 
-#!/usr/bin/python3
-
-'''UTF-8 validator'''
 
 def validUTF8(data):
     """
     check valid UTF-8
     """
+
     c = 0
     for n in data:
         m = 1 << 7
         if not c:
             while n & m:
-                c += 1
+                c = c + 1
                 m >>= 1
             if c > 4:
                 return False
         elif n >> 6 != 2:
             return False
         if c:
-            c -= 1
+            c = c - 1
     return c == 0
