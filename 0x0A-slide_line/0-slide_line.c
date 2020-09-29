0dd8ecbae90c12b579bb9fb8872a99730a637366
kaelwebdev <cdcp-1795@hotmail.com>
2020-09-29 16:40:48 -0500

    start p0x0A-slide_line

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,14 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - slides and merges an array of integers
+ * @line: array (int)
+ * @size: length of array
+ * @direction: direction of merge (L/R)
+ * Return: 1 | 0
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	return 1;
+}
+
