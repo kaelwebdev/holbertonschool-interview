00c2ea42c903f294bf9aa4052b175859c60e0375
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-14 01:36:01 +0000

    start p0x0F. Star Wars API

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+// using star wars API
+
+const request = require('request');
+
+// Request URL
+const URL_BASE = 'https://swapi-api.hbtn.io/api/films';
+
+function doRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+// Usage:
+
+async function main (filmID) {
+  const res = await doRequest(`${URL_BASE}/${filmID}`);
+  for (const e of res.characters) {
+    const pj = await doRequest(e);
+    console.log(pj.name);
+  }
+}
+
+main(3);
